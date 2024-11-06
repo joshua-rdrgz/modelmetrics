0444d912832a6c4104bd6ebdbaa3acd0843f40ac
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2024-11-06 12:58:26 -0600

    adjust loader color on current theme

--- client/src/ui/ComponentLoader.tsx
+++ client/src/ui/ComponentLoader.tsx
@@ -1,9 +1,15 @@
 import { PacmanLoader } from 'react-spinners';
+import { useTheme } from '@/config/theme-provider';
 
 export const ComponentLoader = () => {
+  const { actualTheme } = useTheme();
+
   return (
     <div className='flex justify-center items-center p-32'>
-      <PacmanLoader size={35} />
+      <PacmanLoader
+        size={35}
+        color={actualTheme === 'dark' ? '#ffffff' : '#000000'}
+      />
     </div>
   );
 };
--- client/src/ui/FullScreenLoader.tsx
+++ client/src/ui/FullScreenLoader.tsx
@@ -1,9 +1,15 @@
 import { PacmanLoader } from 'react-spinners';
+import { useTheme } from '@/config/theme-provider';
 
 export const FullScreenLoader = () => {
+  const { actualTheme } = useTheme();
+
   return (
     <div className='min-h-screen min-w-full flex justify-center items-center'>
-      <PacmanLoader size={50} />
+      <PacmanLoader
+        size={50}
+        color={actualTheme === 'dark' ? '#ffffff' : '#000000'}
+      />
     </div>
   );
 };
