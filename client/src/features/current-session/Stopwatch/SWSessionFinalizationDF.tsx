50dbb8ac44c70588ff2ec0aa659f9fdda494f8b9
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2024-11-13 17:56:39 -0600

    add constant evaluation of events validation errors

--- client/src/features/current-session/Stopwatch/SWSessionFinalizationDF.tsx
+++ client/src/features/current-session/Stopwatch/SWSessionFinalizationDF.tsx
@@ -6,6 +6,7 @@ import { Button } from '@/ui/button';
 import * as D from '@/ui/dialog';
 import * as F from '@/ui/form';
 import { Input } from '@/ui/input';
+import * as SA from '@/ui/scroll-area';
 import { validateEvents } from '@/utils/validateEvents';
 import { zodResolver } from '@hookform/resolvers/zod';
 import React, { useCallback, useEffect, useState } from 'react';
@@ -69,6 +70,14 @@ export const SWSessionFinalizationDF: React.FC<
       events,
     },
   });
+
+  // Triggers validation on every change
+  useEffect(() => {
+    const subscription = form.watch(() => {
+      form.trigger('events');
+    });
+    return () => subscription.unsubscribe();
+  }, [form]);
 
   // Reset form when a new session starts
   useEffect(() => {
@@ -141,109 +150,122 @@ export const SWSessionFinalizationDF: React.FC<
           </D.Description>
         </D.Header>
         <F.Root formMethods={form} onSubmit={handleSubmit}>
-          <F.Field
-            control={form.control}
-            name='projectName'
-            render={({ field: { onChange, ...field } }) => (
-              <F.Item>
-                <div className='flex justify-between'>
-                  <F.Label>Project Name</F.Label>
-                  <F.Message />
-                </div>
-                <F.Control>
-                  <Input
-                    {...field}
-                    onChange={(e) => {
-                      onChange(e);
-                      setProjectName(e.target.value);
-                    }}
-                  />
-                </F.Control>
-              </F.Item>
-            )}
-          />
-          <F.Field
-            control={form.control}
-            name='hourlyRate'
-            render={({ field: { onChange, ...field } }) => (
-              <F.Item>
-                <div className='flex justify-between'>
-                  <F.Label>Hourly Rate</F.Label>
-                  <F.Message />
-                </div>
-                <F.Control>
-                  <Input
-                    type='number'
-                    {...field}
-                    onChange={(e) => {
-                      onChange(parseFloat(e.target.value));
-                      setHourlyRate(parseFloat(e.target.value));
-                    }}
-                  />
-                </F.Control>
-              </F.Item>
-            )}
-          />
-          <div className='mt-6 flex justify-end space-x-4'>
-            <Button
-              type='button'
-              onClick={() => setActivePage('stats')}
-              variant={activePage === 'stats' ? 'secondary' : 'outline'}
-            >
-              Session Event Stats
-            </Button>
-            <Button
-              type='button'
-              onClick={() => setActivePage('events')}
-              variant={activePage === 'events' ? 'secondary' : 'outline'}
-            >
-              Adjust Session Events
-            </Button>
-          </div>
-          {/* EVENTS ERROR */}
-          {form.formState.errors.events ? (
-            <p className='text-destructive font-bold text-md m-3 text-center'>
-              {form.formState.errors.events.message}
-            </p>
-          ) : (
-            <></>
-          )}
-          {/* STATS PAGE */}
-          {activePage === 'stats' && (
-            <div className='mt-6'>
-              <h3 className='text-lg font-semibold mb-2'>
+          <SA.Root className='h-[calc(75vh-200px)] pr-4'>
+            <F.Field
+              control={form.control}
+              name='projectName'
+              render={({
+                field: { onChange, ...field },
+                fieldState: { error },
+              }) => (
+                <F.Item className='p-1'>
+                  <div className='flex justify-between'>
+                    <F.Label>Project Name</F.Label>
+                    <F.Message />
+                  </div>
+                  <F.Control>
+                    <Input
+                      {...field}
+                      error={!!error}
+                      onChange={(e) => {
+                        onChange(e);
+                        setProjectName(e.target.value);
+                      }}
+                    />
+                  </F.Control>
+                </F.Item>
+              )}
+            />
+            <F.Field
+              control={form.control}
+              name='hourlyRate'
+              render={({
+                field: { onChange, ...field },
+                fieldState: { error },
+              }) => (
+                <F.Item className='p-1'>
+                  <div className='flex justify-between'>
+                    <F.Label>Hourly Rate</F.Label>
+                    <F.Message />
+                  </div>
+                  <F.Control>
+                    <Input
+                      type='number'
+                      error={!!error}
+                      {...field}
+                      onChange={(e) => {
+                        onChange(parseFloat(e.target.value));
+                        setHourlyRate(parseFloat(e.target.value));
+                      }}
+                    />
+                  </F.Control>
+                </F.Item>
+              )}
+            />
+            <div className='mt-6 flex justify-end space-x-4'>
+              <Button
+                type='button'
+                onClick={() => setActivePage('stats')}
+                variant={activePage === 'stats' ? 'secondary' : 'outline'}
+              >
                 Session Event Stats
-              </h3>
-              <D.Description className='mb-4'>
-                Overview of your session statistics. Review total time, work
-                time, breaks, and completed tasks.
-              </D.Description>
-              <SessionFinalizationStats />
-            </div>
-          )}
-          {/* EVENTS PAGE */}
-          {activePage === 'events' && (
-            <div className='mt-6'>
-              <h3 className='text-lg font-semibold mb-2'>
+              </Button>
+              <Button
+                type='button'
+                onClick={() => setActivePage('events')}
+                variant={activePage === 'events' ? 'secondary' : 'outline'}
+              >
                 Adjust Session Events
-              </h3>
-              <D.Description className='mb-4'>
-                Review and modify your session events. Adjust timestamps or
-                event types if needed.
-              </D.Description>
-              <AdjustableEvents
-                events={events}
-                onEventChange={handleEventChange}
-                editingEventIndex={editingEventIndex}
-                setEditingEventIndex={setEditingEventIndex}
-              />
+              </Button>
             </div>
-          )}
+            {/* EVENTS ERROR */}
+            {form.formState.errors.events ? (
+              <p className='text-destructive font-bold text-md m-3 text-center'>
+                {form.formState.errors.events.message}
+              </p>
+            ) : (
+              <></>
+            )}
+            {/* STATS PAGE */}
+            {activePage === 'stats' && (
+              <div className='mt-6'>
+                <h3 className='text-lg font-semibold mb-2'>
+                  Session Event Stats
+                </h3>
+                <D.Description className='mb-4'>
+                  Overview of your session statistics. Review total time, work
+                  time, breaks, and completed tasks.
+                </D.Description>
+                <SessionFinalizationStats />
+              </div>
+            )}
+            {/* EVENTS PAGE */}
+            {activePage === 'events' && (
+              <div className='mt-6'>
+                <h3 className='text-lg font-semibold mb-2'>
+                  Adjust Session Events
+                </h3>
+                <D.Description className='mb-4'>
+                  Review and modify your session events. Adjust timestamps or
+                  event types if needed.
+                </D.Description>
+                <AdjustableEvents
+                  events={events}
+                  onEventChange={handleEventChange}
+                  editingEventIndex={editingEventIndex}
+                  setEditingEventIndex={setEditingEventIndex}
+                />
+              </div>
+            )}
+          </SA.Root>
           <D.Footer className='mt-6'>
             <Button type='button' variant='outline' onClick={handleCancel}>
               Cancel
             </Button>
-            <Button type='submit' disabled={editingEventIndex !== null}>
+            <Button
+              type='submit'
+              disabled={!form.formState.isValid || editingEventIndex !== null}
+            >
               Submit
             </Button>
           </D.Footer>
