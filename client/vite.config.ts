754b6ef69c78ab1e7f44bc6cc2ab7c3a3d922603
Joshua Rodriguez <joshuarodriguezcoding@gmail.com>
2025-04-17 13:44:53 -0500

    switch CI/CD client pipeline to node version 22

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,6 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { webcrypto } from 'node:crypto';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,13 +21,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
-  },
-  // Attempt solving crypto issue being undefined
-  // in CI/CD pipeline
-  define: {
-    global: {
-      crypto: webcrypto,
     },
   },
 });
